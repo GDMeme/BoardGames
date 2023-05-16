e568564e7867d8a83b39be57220aa4822b9b59e7
Matthew Li <m739li@uwaterloo.ca>
2023-05-16 18:40:11 -0400

    Some more progress

--- MachiKoro JS/player.js
+++ MachiKoro JS/player.js
@@ -3,5 +3,7 @@ export class Player {
         this.balance = 0;
         this.establishments = Array(15).fill(0);
         this.landmarks = Array(4).fill(false);
+        this.establishments[0] = 1; // 1 wheat field
+        this.establishments[2] = 1; // 1 bakery
     }
 }
--- MachiKoro JS/start.js
+++ MachiKoro JS/start.js
@@ -8,6 +8,7 @@ export function start(numberofplayers) {
     numberofplayers = parseInt(numberofplayers);
     document.getElementById('beforegametext').style.display = "none";
     document.getElementById('startgametext').style.display = "inline";
+    document.getElementById('endturnbutton').style.display = "inline"; // show the end turn button
     const players = [];
     for (let i = 0; i < numberofplayers; i++) {
         players.push(new Player());
@@ -15,16 +16,27 @@ export function start(numberofplayers) {
 
     let playerCounter = 0; // * * playerCounter is 0 indexed!!!
 
-    document.getElementById('rolldicebutton').onclick = function () {
-        document.getElementById('rolldicebutton').disabled = true;
-        document.getElementById('endturnbutton').disabled = false; // add two dice possibility if train station unlocked
+    document.getElementById('roll1dicebutton').onclick = function () {
+        document.getElementById('roll1dicebutton').disabled = true; // disable the roll dice button
+        document.getElementById('roll2dicebutton').disabled = true; // disable the other roll dice button
+        document.getElementById('endturnbutton').disabled = false; // enable the end turn button
+
         let rollNumber = Math.floor(Math.random() * 6 + 1); // generates number between 1 and 6 inclusive
         document.querySelector('#rollnumber').innerHTML = 'You rolled a ' + rollNumber;
         document.getElementById('rollnumber').style.display = "inline";
         document.getElementById('buyestablishment').style.display = "inline";
     }
 
+    document.getElementById('roll2dicebutton').onclick = function () {
+        document.getElementById('roll1dicebutton').disabled = true; // disable the roll dice button
+        document.getElementById('roll2dicebutton').disabled = true; // disable the other roll dice button
+        document.getElementById('endturnbutton').disabled = false; // enable the end turn button
 
+        let rollNumber = Math.floor(Math.random() * 12 + 1); // generates number between 1 and 12 inclusive
+        document.querySelector('#rollnumber').innerHTML = 'You rolled a ' + rollNumber;
+        document.getElementById('rollnumber').style.display = "inline";
+        document.getElementById('buyestablishment').style.display = "inline"; // let player buy an establishment
+    }
 
     document.getElementById('endturnbutton').onclick = function() {
         playerCounter++;
@@ -32,7 +44,7 @@ export function start(numberofplayers) {
             playerCounter = 0;
         }
         document.querySelector('#playerturn').innerHTML = 'Player ' + (playerCounter + 1) + '\'s turn!';
-        document.getElementById('rolldicebutton').disabled = false;
+        document.getElementById('roll1dicebutton').disabled = false;
         document.getElementById('endturnbutton').disabled = true;
         document.getElementById('rollnumber').style.display = "none";
         
