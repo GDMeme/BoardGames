058dc86ebcde13af3153cdfa0d35d8985daa5dcb
Matthew Li <m739li@uwaterloo.ca>
2023-05-18 04:01:56 -0400

    enable/disable buttons for shop

---
+++ MachiKoro JS/enableShop.js
@@ -0,0 +1,217 @@
+export function enableShop(balance) {
+    if (balance === 0) {
+        document.getElementById('buywheatfieldbutton').disabled = true;
+        document.getElementById('buyranchbutton').disabled = true;
+        document.getElementById('buybakerybutton').disabled = true;
+        document.getElementById('buycafebutton').disabled = true;
+        document.getElementById('buyconveniencestorebutton').disabled = true;
+        document.getElementById('buyforestbutton').disabled = true;
+        document.getElementById('buystadiumbutton').disabled = true;
+        document.getElementById('buytvstationbutton').disabled = true;
+        document.getElementById('buybusinesscenterbutton').disabled = true;
+        document.getElementById('buycheesefactorybutton').disabled = true;
+        document.getElementById('buyfurniturefactorybutton').disabled = true;
+        document.getElementById('buyminebutton').disabled = true;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = true;
+        document.getElementById('buyappleorchardbutton').disabled = true;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = true;
+
+        document.getElementById('buytrainstationbutton').disabled = true;
+        document.getElementById('buyshoppingmallbutton').disabled = true;
+        document.getElementById('buyamusementparkbutton').disabled = true;
+        document.getElementById('buyradiotowerbutton').disabled = true;
+    }
+    if (balance === 1) {
+        document.getElementById('buywheatfieldbutton').disabled = false;
+        document.getElementById('buyranchbutton').disabled = false;
+        document.getElementById('buybakerybutton').disabled = false;
+
+        document.getElementById('buycafebutton').disabled = true;
+        document.getElementById('buyconveniencestorebutton').disabled = true;
+        document.getElementById('buyforestbutton').disabled = true;
+        document.getElementById('buystadiumbutton').disabled = true;
+        document.getElementById('buytvstationbutton').disabled = true;
+        document.getElementById('buybusinesscenterbutton').disabled = true;
+        document.getElementById('buycheesefactorybutton').disabled = true;
+        document.getElementById('buyfurniturefactorybutton').disabled = true;
+        document.getElementById('buyminebutton').disabled = true;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = true;
+        document.getElementById('buyappleorchardbutton').disabled = true;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = true;
+
+        document.getElementById('buytrainstationbutton').disabled = true;
+        document.getElementById('buyshoppingmallbutton').disabled = true;
+        document.getElementById('buyamusementparkbutton').disabled = true;
+        document.getElementById('buyradiotowerbutton').disabled = true;
+    } else if (balance === 2) {
+        document.getElementById('buywheatfieldbutton').disabled = false;
+        document.getElementById('buyranchbutton').disabled = false;
+        document.getElementById('buybakerybutton').disabled = false;
+        document.getElementById('buycafebutton').disabled = false;
+        document.getElementById('buyconveniencestorebutton').disabled = false;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = false;
+
+        document.getElementById('buyforestbutton').disabled = true;
+        document.getElementById('buystadiumbutton').disabled = true;
+        document.getElementById('buytvstationbutton').disabled = true;
+        document.getElementById('buybusinesscenterbutton').disabled = true;
+        document.getElementById('buycheesefactorybutton').disabled = true;
+        document.getElementById('buyfurniturefactorybutton').disabled = true;
+        document.getElementById('buyminebutton').disabled = true;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = true;
+        document.getElementById('buyappleorchardbutton').disabled = true;
+
+        document.getElementById('buytrainstationbutton').disabled = true;
+        document.getElementById('buyshoppingmallbutton').disabled = true;
+        document.getElementById('buyamusementparkbutton').disabled = true;
+        document.getElementById('buyradiotowerbutton').disabled = true;
+    } else if (balance === 3) {
+        document.getElementById('buywheatfieldbutton').disabled = false;
+        document.getElementById('buyranchbutton').disabled = false;
+        document.getElementById('buybakerybutton').disabled = false;
+        document.getElementById('buycafebutton').disabled = false;
+        document.getElementById('buyconveniencestorebutton').disabled = false;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = false;
+        document.getElementById('buyforestbutton').disabled = false;
+        document.getElementById('buyfurniturefactorybutton').disabled = false;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = false;
+        document.getElementById('buyappleorchardbutton').disabled = false;
+
+        document.getElementById('buystadiumbutton').disabled = true;
+        document.getElementById('buytvstationbutton').disabled = true;
+        document.getElementById('buybusinesscenterbutton').disabled = true;
+        document.getElementById('buycheesefactorybutton').disabled = true;
+        document.getElementById('buyminebutton').disabled = true;
+
+        document.getElementById('buytrainstationbutton').disabled = true;
+        document.getElementById('buyshoppingmallbutton').disabled = true;
+        document.getElementById('buyamusementparkbutton').disabled = true;
+        document.getElementById('buyradiotowerbutton').disabled = true;
+    } else if (balance === 4) {
+        document.getElementById('buywheatfieldbutton').disabled = false;
+        document.getElementById('buyranchbutton').disabled = false;
+        document.getElementById('buybakerybutton').disabled = false;
+        document.getElementById('buycafebutton').disabled = false;
+        document.getElementById('buyconveniencestorebutton').disabled = false;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = false;
+        document.getElementById('buyforestbutton').disabled = false;
+        document.getElementById('buyfurniturefactorybutton').disabled = false;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = false;
+        document.getElementById('buyappleorchardbutton').disabled = false;
+
+        document.getElementById('buystadiumbutton').disabled = true;
+        document.getElementById('buytvstationbutton').disabled = true;
+        document.getElementById('buybusinesscenterbutton').disabled = true;
+        document.getElementById('buycheesefactorybutton').disabled = true;
+        document.getElementById('buyminebutton').disabled = true;
+
+        document.getElementById('buytrainstationbutton').disabled = false;
+        document.getElementById('buyshoppingmallbutton').disabled = true;
+        document.getElementById('buyamusementparkbutton').disabled = true;
+        document.getElementById('buyradiotowerbutton').disabled = true;
+    } else if (balance === 5) {
+        document.getElementById('buywheatfieldbutton').disabled = false;
+        document.getElementById('buyranchbutton').disabled = false;
+        document.getElementById('buybakerybutton').disabled = false;
+        document.getElementById('buycafebutton').disabled = false;
+        document.getElementById('buyconveniencestorebutton').disabled = false;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = false;
+        document.getElementById('buyforestbutton').disabled = false;
+        document.getElementById('buycheesefactorybutton').disabled = false;
+        document.getElementById('buyfurniturefactorybutton').disabled = false;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = false;
+        document.getElementById('buyappleorchardbutton').disabled = false;
+
+        document.getElementById('buystadiumbutton').disabled = true;
+        document.getElementById('buytvstationbutton').disabled = true;
+        document.getElementById('buybusinesscenterbutton').disabled = true;
+        document.getElementById('buyminebutton').disabled = true;
+
+        document.getElementById('buytrainstationbutton').disabled = false;
+        document.getElementById('buyshoppingmallbutton').disabled = true;
+        document.getElementById('buyamusementparkbutton').disabled = true;
+        document.getElementById('buyradiotowerbutton').disabled = true;
+    } else if (balance === 6) {
+        document.getElementById('buywheatfieldbutton').disabled = false;
+        document.getElementById('buyranchbutton').disabled = false;
+        document.getElementById('buybakerybutton').disabled = false;
+        document.getElementById('buycafebutton').disabled = false;
+        document.getElementById('buyconveniencestorebutton').disabled = false;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = false;
+        document.getElementById('buyforestbutton').disabled = false;
+        document.getElementById('buystadiumbutton').disabled = false;
+        document.getElementById('buycheesefactorybutton').disabled = false;
+        document.getElementById('buyfurniturefactorybutton').disabled = false;
+        document.getElementById('buyminebutton').disabled = false;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = false;
+        document.getElementById('buyappleorchardbutton').disabled = false;
+
+        document.getElementById('buytvstationbutton').disabled = true;
+        document.getElementById('buybusinesscenterbutton').disabled = true;
+
+        document.getElementById('buytrainstationbutton').disabled = false;
+        document.getElementById('buyshoppingmallbutton').disabled = true;
+        document.getElementById('buyamusementparkbutton').disabled = true;
+        document.getElementById('buyradiotowerbutton').disabled = true;
+    } else if (balance === 7) {
+        document.getElementById('buywheatfieldbutton').disabled = false;
+        document.getElementById('buyranchbutton').disabled = false;
+        document.getElementById('buybakerybutton').disabled = false;
+        document.getElementById('buycafebutton').disabled = false;
+        document.getElementById('buyconveniencestorebutton').disabled = false;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = false;
+        document.getElementById('buyforestbutton').disabled = false;
+        document.getElementById('buystadiumbutton').disabled = false;
+        document.getElementById('buytvstationbutton').disabled = false;
+        document.getElementById('buycheesefactorybutton').disabled = false;
+        document.getElementById('buyfurniturefactorybutton').disabled = false;
+        document.getElementById('buyminebutton').disabled = false;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = false;
+        document.getElementById('buyappleorchardbutton').disabled = false;
+
+        document.getElementById('buybusinesscenterbutton').disabled = true;
+
+        document.getElementById('buytrainstationbutton').disabled = false;
+        document.getElementById('buyshoppingmallbutton').disabled = true;
+        document.getElementById('buyamusementparkbutton').disabled = true;
+        document.getElementById('buyradiotowerbutton').disabled = true;
+    } else if (balance > 7) {
+        document.getElementById('buywheatfieldbutton').disabled = false;
+        document.getElementById('buyranchbutton').disabled = false;
+        document.getElementById('buybakerybutton').disabled = false;
+        document.getElementById('buycafebutton').disabled = false;
+        document.getElementById('buyconveniencestorebutton').disabled = false;
+        document.getElementById('buyfruitandvegetablemarketbutton').disabled = false;
+        document.getElementById('buyforestbutton').disabled = false;
+        document.getElementById('buystadiumbutton').disabled = false;
+        document.getElementById('buytvstationbutton').disabled = false;
+        document.getElementById('buybusinesscenterbutton').disabled = false;
+        document.getElementById('buycheesefactorybutton').disabled = false;
+        document.getElementById('buyfurniturefactorybutton').disabled = false;
+        document.getElementById('buyminebutton').disabled = false;
+        document.getElementById('buyfamilyrestaurantbutton').disabled = false;
+        document.getElementById('buyappleorchardbutton').disabled = false;
+
+        document.getElementById('buytrainstationbutton').disabled = false;
+        
+        if (balance >= 10) {
+            document.getElementById('buyshoppingmallbutton').disabled = false;
+            if (balance >= 16) {
+                document.getElementById('buyamusementparkbutton').disabled = false;
+                if (balance >= 22) {
+                    document.getElementById('buyradiotowerbutton').disabled = false;
+                } else {
+                    document.getElementById('buyradiotowerbutton').disabled = true;
+                }
+            } else {
+                document.getElementById('buyamusementparkbutton').disabled = true;
+                document.getElementById('buyradiotowerbutton').disabled = true;
+            }
+        } else {
+            document.getElementById('buyshoppingmallbutton').disabled = true;
+            document.getElementById('buyamusementparkbutton').disabled = true;
+            document.getElementById('buyradiotowerbutton').disabled = true;
+        }
+    }
+    
+}
