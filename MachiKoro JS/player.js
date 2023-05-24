b82f327467aa7f5a84a0606d4cf82c4bae6c0717
Matthew Li <m739li@uwaterloo.ca>
2023-05-24 19:44:03 -0400

    reminder to change initial player's balance

--- MachiKoro JS/player.js
+++ MachiKoro JS/player.js
@@ -1,6 +1,6 @@
 export class Player {
     constructor() {
-        this.balance = 10000; // change this back to 0
+        this.balance = 10000; // TODO: change this back to 0
         this.establishments = Array(15).fill(0);
         this.landmarks = Array(4).fill(false);
         this.establishments[0] = 1; // 1 wheat field
