0c13d3d25e707dfae337a92c93a95db6ab7b207e
Matthew Li <m739li@uwaterloo.ca>
2023-05-24 19:43:54 -0400

    Some progress on income

--- MachiKoro JS/income.js
+++ MachiKoro JS/income.js
@@ -1,45 +1,57 @@
-// remember to include shopping mall
+// TODO: remember to include shopping mall
 
-export function income(roll, players, playerCounter) {
+// TODO: remember to add/change html stuff
+
+export function income(roll, players, playerCounter, buildings) {
     // red activate
     if (roll === 3) {
-
-    }
-    if (roll === 10) {
-
-    }
-
-    // green and blue activate
-
-    // 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12
-
-    // purple activate
-
-    // 6, 7, 8
-
-    if (roll === 1) {
-
-    } else if (roll === 2) {
-
-    } else if (roll === 3) {
-
-    } else if (roll === 4) {
-
-    } else if (roll === 5) {
-
-    } else if (roll === 6) {
-
-    } else if (roll === 7) {
-
-    } else if (roll === 8) {
-
-    } else if (roll === 9) {
-
+        let index = playerCounter; // * * index is 0 indexed!!
+        while (players[playerCounter].balance > 0) {
+            index--;
+            if (index === -1) {
+                index = players.length - 1;
+            } 
+            if (index === playerCounter) {
+                break;
+            }
+            let numberOfCafe = players[index].establishments[3]
+            let moneyOwed = players[index].landmarks[1] ? (2 * numberOfCafe) : numberOfCafe; // shopping mall
+            if (moneyOwed > players[playerCounter].balance) {
+                moneyOwed = players[playerCounter].balance
+            }
+            players[playerCounter].balance -= moneyOwed;
+            players[index].balance += moneyOwed;
+        }
     } else if (roll === 10) {
 
-    } else if (roll === 11) {
+    }
 
-    } else if (roll === 12) {
+    // TODO: purple activate
 
-    } 
+    let activated_buildings;
+    let currentPlayer = 0;
+    for (const player of players) {
+        if (currentPlayer === playerCounter) {
+            // will only return green and blue buildings
+            activated_buildings = buildings.map((building, buildingIndex) => building.trigger?.includes(roll) ? buildingIndex : undefined).filter(x => x !== undefined); 
+        } else {
+            activated_buildings = buildings.map((building, buildingIndex) => (building.trigger?.includes(roll) && building.colour === 'blue') ? buildingIndex : undefined).filter(x => x !== undefined);
+        }
+        for (const buildingIndex of activated_buildings) {
+            if (Array.isArray(buildings[buildingIndex].income)) { // shopping mall doesn't apply here
+                let numberOfSpecial = 0;
+                for (const incomeIndex of buildings[buildingIndex.income]) {
+                    numberOfSpecial += player.establishments[incomeIndex];
+                }
+                player.balance += buildings[buildingIndex].multiplier * numberOfSpecial;
+            } else {
+                if (buildings[buildingIndex].shopping === true && player.landmarks[1]) { // shopping mall
+                player.balance += player.establishments[buildingIndex] * (buildings[buildingIndex].income + 1);
+                } else {
+                player.balance += player.establishments[buildingIndex] * buildings[buildingIndex].income;
+                }
+            }
+        }
+        currentPlayer++;
+    }
 }
--- MachiKoro JS/start.js
+++ MachiKoro JS/start.js
@@ -5,21 +5,21 @@ import { buy, enableShop } from './shop.js';
 import { income } from './income.js';
 
 const buildings = [
-    {name: 'wheatfield', displayName: 'Wheat Field', cost: 1},
-    {name: 'ranch', displayName: 'Ranch', cost: 1},
-    {name: 'bakery', displayName: 'Bakery', cost: 1},
-    {name: 'cafe', displayName: 'Cafe', cost: 2},
-    {name: 'conveniencestore', displayName: 'Convenience Store', cost: 2},
-    {name: 'forest', displayName: 'Forest', cost: 3},
-    {name: 'stadium', displayName: 'Stadium', cost: 6},
+    {name: 'wheatfield', displayName: 'Wheat Field', cost: 1, income: 1, shopping: false, colour: 'blue', trigger: [1]},
+    {name: 'ranch', displayName: 'Ranch', cost: 1, income: 1, shopping: false, colour: 'blue', trigger: [2]},
+    {name: 'bakery', displayName: 'Bakery', cost: 1, income: 1, shopping: true, trigger: [2, 3]}, // if colour is not blue, it must be green
+    {name: 'cafe', displayName: 'Cafe', cost: 2}, // red is special case
+    {name: 'conveniencestore', displayName: 'Convenience Store', cost: 2, income: 3, shopping: true, trigger: [4]},
+    {name: 'forest', displayName: 'Forest', cost: 3, income: 1, shopping: false, colour: 'blue', trigger: [5]},
+    {name: 'stadium', displayName: 'Stadium', cost: 6}, // purples are special case
     {name: 'tvstation', displayName: 'TV Station', cost: 7},
     {name: 'businesscenter', displayName: 'Business Center', cost: 8},
-    {name: 'cheesefactory', displayName: 'Cheese Factory', cost: 5},
-    {name: 'furniturefactory', displayName: 'Furniture Factory', cost: 3},
-    {name: 'mine', displayName: 'Mine', cost: 6},
-    {name: 'familyrestaurant', displayName: 'Family Restaurant', cost: 3},
-    {name: 'appleorchard', displayName: 'Apple Orchard', cost: 3},
-    {name: 'fruitandvegetablemarket', displayName: 'Fruit and Vegetable Market', cost: 2},
+    {name: 'cheesefactory', displayName: 'Cheese Factory', cost: 5, income: [1], multiplier: 3, trigger: [7]}, // don't need shopping since income is array
+    {name: 'furniturefactory', displayName: 'Furniture Factory', cost: 3, income: [5, 11], multiplier: 3, trigger: [8]},
+    {name: 'mine', displayName: 'Mine', cost: 6, income: 5, shopping: false, colour: 'blue', trigger: [9]},
+    {name: 'familyrestaurant', displayName: 'Family Restaurant', cost: 3}, // red is special case
+    {name: 'appleorchard', displayName: 'Apple Orchard', cost: 3, income: 3, shopping: false, colour: 'blue', trigger: [10]},
+    {name: 'fruitandvegetablemarket', displayName: 'Fruit and Vegetable Market', cost: 2, income: [0, 13], multiplier: 2, trigger: [11, 12]},
     {name: 'trainstation', displayName: 'Train Station', cost: 4},
     {name: 'shoppingmall', displayName: 'Shopping Mall', cost: 10},
     {name: 'amusementpark', displayName: 'Amusement Park', cost: 16},
@@ -54,7 +54,7 @@ function playerTurn(players, playerCounter, flag) {
     document.getElementById('rollnumber').style.display = "inline";
 
     // everyone collects income
-    income(rollNumber, players, playerCounter);
+    income(rollNumber, players, playerCounter, buildings);
 
     // buy establishment/landmark
     document.getElementById('buysomething').style.display = "inline";
@@ -105,7 +105,7 @@ export function start(numberofplayers) {
     }
 
     document.getElementById('endturnbutton').onclick = function() {
-        if (document.getElementById('rolldoubles').style.display != "inline") { // amusement park did not activate
+        if (document.getElementById('rolldoubles').style.display !== "inline") { // amusement park did not activate
             playerCounter++;
         }
         if (playerCounter === numberofplayers) {
