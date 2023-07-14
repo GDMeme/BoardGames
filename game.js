4ba0188bfc6515a94f08641e37c0d791b3474bf4
Matthew Li <m739li@uwaterloo.ca>
2023-07-14 03:31:20 -0400

    removed logs

--- game.js
+++ game.js
@@ -2,14 +2,12 @@ import { Player } from './player.js';
 
 export class Game {
     constructor(numberOfPlayers, playerNames) {
-        console.log('playerNames is ', playerNames);
         this.players = Array(numberOfPlayers);
         this.playerNames = Array(numberOfPlayers);
         for (let i = 0; i < numberOfPlayers; i++) {
             this.players[i] = new Player();
             this.playerNames[i] = playerNames[i];
         }
-        console.log('now playerNames is ', this.playerNames);
         this.playerCounter = 0; // * * playerCounter is 0 indexed!!
         this.numberOfPlayers = numberOfPlayers;
     }
