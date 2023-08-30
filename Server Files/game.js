79ec1720a1ec0b7c87bfd092286e93ad1bc4037b
Matthew Li <m739li@uwaterloo.ca>
2023-08-30 00:05:14 -0400

    Better variable name

--- Server Files/game.js
+++ Server Files/game.js
@@ -5,11 +5,11 @@ export class Game {
         let ID, name, numberOfPlayers;
 
         let state = 0;
-        let TVStationActivated = 0; 
+        let TVStationActivatedState = 0; 
         // * * 0: TV Station did not activate
         // * * 1: TV Station activated, did not steal coins yet
         // * * 2: TV Station activated, already stole coins
-        let businessCenterActivated = 0;
+        let businessCenterActivatedState = 0;
         let businessTargetPlayerIndex = -1;
         let businessReceiveIndex = -1;
         let income;
